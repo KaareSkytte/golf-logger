04180809d4cf61f1b24f5c37a41263d46e77dfcf
KaareSkytte <kaareskytte1431@gmail.com>
2025-07-29 10:35:13 +0200

    Added login functions to gui

--- gui/main_gui.go
+++ gui/main_gui.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -20,7 +26,45 @@ func main() {
 	errorLabel := widget.NewLabel("")
 
 	loginBtn := widget.NewButton("Login", func() {
-		errorLabel.SetText("Not implemented yet")
+		email := emailEntry.Text
+		password := passwordEntry.Text
+
+		payload := map[string]string{
+			"email":    email,
+			"password": password,
+		}
+
+		data, err := json.Marshal(payload)
+		if err != nil {
+			errorLabel.SetText("Error encoding credentials")
+			return
+		}
+
+		go func() {
+			resp, err := http.Post("http://localhost:8080/api/login", "application/json", bytes.NewBuffer(data))
+			if err != nil {
+				myApp.SendNotification(&fyne.Notification{Title: "Error", Content: "Unable to reach server"})
+				return
+			}
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			if resp.StatusCode != 200 {
+				errorLabel.SetText(fmt.Sprintf("Login failed: %s", body))
+				return
+			}
+
+			var loginResp struct {
+				UserID    string `json:"user_id"`
+				AuthToken string `json:"auth_token"`
+			}
+			if err := json.Unmarshal(body, &loginResp); err != nil {
+				errorLabel.SetText("Bad response from server")
+				return
+			}
+
+			errorLabel.SetText("Login Succesful!")
+		}()
 	})
 
 	form := container.NewVBox(
